71a33c46eee5139804d957f568bc024c9c64c175
Aritra <aritrabiswas9788@gmail.com>
2025-03-11 14:56:16 +0530

    Convex Clerk Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size="sm">
-      Click Me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+      This is a page for authenticated users.
+      </div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://prime-mako-71.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
